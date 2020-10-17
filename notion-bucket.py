f9767a0daf9a13ac9d0eb89629bc82cbc41a5f03
Shane Lee <me@shanel.ee>
2020-10-17 15:44:21 +0100

    Remove print statement

--- notion-bucket.py
+++ notion-bucket.py
@@ -2,7 +2,6 @@ from notion.client import NotionClient
 
 with open('token.txt') as file:
   token = file.read().strip('\n')
-  print(token)
   client = NotionClient(token_v2=token)
   cv = client.get_collection_view('https://www.notion.so/1824799b019e44e3a5a98a655f4e9d49?v=27b099ae037f4907b6f55f0f73242c98')
 
