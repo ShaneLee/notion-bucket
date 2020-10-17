cfaa56adbb68cefaca9eb735ffee35e2786a29e9
Shane Lee <me@shanel.ee>
2020-10-17 16:55:44 +0100

    Make path work regardless of where the script is called from

--- notion-bucket.py
+++ notion-bucket.py
@@ -1,6 +1,7 @@
+import os, sys
 from notion.client import NotionClient
 
-with open('token.txt') as file:
+with open(os.path.join(sys.path[0], 'token.txt')) as file:
   token = file.read().strip('\n')
   client = NotionClient(token_v2=token)
   cv = client.get_collection_view('https://www.notion.so/1824799b019e44e3a5a98a655f4e9d49?v=27b099ae037f4907b6f55f0f73242c98')
